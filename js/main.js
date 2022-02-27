b5ca636a04625ee9e7493f750928dcc713e8c047
AliAlTaweel <engalihaitham@gmail.com>
2022-02-27 22:17:25 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 let mainSec = document.querySelector(".main-sec");
 
 // Get Array of Imges
-let imgArray = ["/imges/01.jpg","/imges/02.jpg", "/imges/03.jpg", "/imges/04.jpg", "/imges/05.jpg" ];
+let imgArray = ["./imges/01.jpg","./imges/02.jpg", "./imges/03.jpg", "./imges/04.jpg", "./imges/05.jpg" ];
 
 //Get Random Number
 let randomNum = Math.floor(Math.random() * imgArray.length);
